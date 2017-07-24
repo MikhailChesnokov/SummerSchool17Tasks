880d6a7971a603c083a6d6e736f2cb333dce55d5
MikhailChesnokov <89194485838@mail.ru>
2017-07-25 01:22:58 +0500

    Correct brackets sequence

---
+++ task1.php
@@ -0,0 +1,105 @@
+﻿<?php
+	class Brackets 
+	{
+	public function isBracketSequenceCorrect($str) 
+	{
+		$true = 'true';
+		$false = 'false';
+		$strlen = strlen($str);
+		
+		if ($strlen % 2 == 1)
+			return $false;
+		
+		$stack = array();
+		$stackSize = 0;
+		$openBrackets = array('{','[','(');
+		$closeBrackets = array('}',']',')');
+		$bracketsMatches = array(
+		')'=>'(',
+		'}'=>'{',
+		']'=>'[');
+
+		for ($i = 0; $i < $strlen; $i++) {
+			if (in_array($str[$i], $openBrackets)) 
+			{	
+				array_push($stack, $str[$i]);
+				$stackSize++;
+			}
+			else
+			if (in_array($str[$i], $closeBrackets))
+			{		
+				if ($stackSize-- == 0)
+					return $false;
+				
+				if ($bracketsMatches[$str[$i]] != array_pop($stack))
+					return $false;
+			}
+			else 
+				return $false;
+		}
+		
+		if ($stackSize == 0)
+			return $true;
+
+		return $false;
+	}
+	
+	/*
+	public function isBracketSequenceCorrectTEST($str)
+	{
+		return strlen($str) > strlen($str = str_replace(array('{}','()','[]') , '' , $str)) ? $this->isBracketSequenceCorrectTEST($str) : strlen($str)==0;	
+	}
+	public function TEST($str) {
+		echo $str.' ';
+		if ($this->isBracketSequenceCorrect($str)=='true' && $this->isBracketSequenceCorrectTEST($str) ||
+			$this->isBracketSequenceCorrect($str)=='false' && !$this->isBracketSequenceCorrectTEST($str))
+			echo 'OK<br />';
+			else 
+			echo 'NOT OK<br />';
+	}
+	*/
+	
+	}
+	
+	/*
+	$obj = new Brackets();
+	$obj->TEST('()');
+	$obj->TEST('abc');
+	$obj->TEST('');
+	$obj->TEST('(');
+	$obj->TEST(')');
+	$obj->TEST('((');
+	$obj->TEST('()');
+	$obj->TEST('))');
+	$obj->TEST(')(');
+	$obj->TEST('[]');
+	$obj->TEST('}{');
+	$obj->TEST('{}');
+	$obj->TEST('(((');
+	$obj->TEST(')))');
+	$obj->TEST('((((');
+	$obj->TEST('((()');
+	$obj->TEST('(()(');
+	$obj->TEST('(())');
+	$obj->TEST('()((');
+	$obj->TEST('()()');
+	$obj->TEST('()))');
+	$obj->TEST(')(((');
+	$obj->TEST(')(()');
+	$obj->TEST(')()(');
+	$obj->TEST(')())');
+	$obj->TEST('))((');
+	$obj->TEST('))()');
+	$obj->TEST(')))(');
+	$obj->TEST('))))');
+	
+	$brackets = array('(','{','[',')','}',']');
+	for ($i1=0; $i1<6; $i1++) {
+		for ($i2=0; $i2<6; $i2++) {
+			for ($i3=0; $i3<6; $i3++) {
+				for ($i4=0; $i4<6; $i4++) {
+					$test = $brackets[$i1].$brackets[$i2].$brackets[$i3].$brackets[$i4];
+					$obj->TEST($test);
+	}}}}
+	*/
+?>
---
+++ task2.php
@@ -0,0 +1,34 @@
+﻿<?php
+	class MaximumUniqueSubstring
+	{
+		public function findMaximumUniqueSubstring($str)
+		{
+			$strlen = strlen($str);
+			$set = array();
+			$startIndex = 0;
+			$endIndex = 0;
+			$maxSubstringLength = 0;
+			$maxSubstringIndex = 0;
+			
+			while ($endIndex < $strlen)
+			{
+				if (!empty($set[$str[$endIndex]]) && $set[$str[$endIndex]] > $startIndex)
+				{
+					$startIndex = $set[$str[$endIndex]];
+				}
+				if ($endIndex - $startIndex + 1 > $maxSubstringLength)
+				{
+					$maxSubstringLength = $endIndex - $startIndex + 1;
+					$maxSubstringIndex = $startIndex;
+				}
+				$set[$str[$endIndex]] = $endIndex + 1;
+				$endIndex++;
+			}
+
+			return substr($str, $maxSubstringIndex, $maxSubstringLength);
+		}
+	}
+	
+	//$obj = new MaximumUniqueSubstring();
+	//echo $obj->findMaximumUniqueSubstring('aabcdAbc');
+?>
